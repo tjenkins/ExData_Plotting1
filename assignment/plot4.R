eccbc742b9ae3fb437c961033d79e8f436b157d1
Tim Jenkins <jenks00@gmail.com>
2014-08-07 23:11:20 -0400

    add zipped data file and code to download/unzip data if necessary to each R script

--- assignment/plot4.R
+++ assignment/plot4.R
@@ -1,6 +1,15 @@
 # Set correct working directory
 setwd("/Users/tjenkins/Workspace/gettingandcleaningdata_coursera/ExData_Plotting1")
 
+# Check if data exists and download/unzip if necessary
+if(!file.exists("data/household_power_consumption.txt")) {
+  if(!file.exists("data/household_power_consumption.zip")) {
+    fileUrl="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(fileUrl, "data/household_power_consumption.zip", method = "curl")  
+  }
+  unzip("data/household_power_consumption.zip",exdir = "data")
+}
+
 # Read data
 dat <- read.table("data/household_power_consumption.txt", header = TRUE, sep=";", stringsAsFactors=FALSE, na.strings = "?", colClasses = c("character","character",rep("numeric",7)))
 
